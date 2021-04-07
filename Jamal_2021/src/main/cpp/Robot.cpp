4749a9b51e4bc644a3fb7d0b99dcfb12caa5dc4d
Aiden Engel <crunchman45@gmail.com>
2021-04-06 20:06:11 -0500

    Added some epic Auto code, shooter, and fixed driving.

--- Jamal_2021/src/main/cpp/Robot.cpp
+++ Jamal_2021/src/main/cpp/Robot.cpp
@@ -8,17 +8,24 @@
 #include "math.h"
 #include <frc/XboxController.h>
 #include <frc/Victor.h>
+#include <rev/CANSparkMax.h>
+#include <cameraserver/CameraServer.h>
+  
 
 frc::XboxController Pogger1(0);
-frc::Victor PepeLeft(1);
-frc::Victor PepeRight(0);
+frc::Victor PepeLeft(3);
+frc::Victor PepeRight(2);
+frc::Victor NotButtShooter(8);
+frc::Victor BaesdRatioCringedexer(5);
 
 void Robot::RobotInit() {
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
 
 
+
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+  frc::CameraServer::GetInstance()->StartAutomaticCapture();
 }
 
 void Robot::RobotPeriodic() {
@@ -26,6 +33,20 @@ void Robot::RobotPeriodic() {
 }
 
 void Robot::AutonomousInit() {
+
+  PepeLeft.Set(0.5);
+  PepeRight.Set(-0.5);
+  frc::Wait(3.96969420); //blame joe for this monstrosity.
+  PepeLeft.Set(0);
+  PepeRight.Set(0);
+  frc::Wait(3);
+  NotButtShooter.Set (1);
+  frc::Wait(2);
+  BaesdRatioCringedexer.Set(0.7);
+  frc::Wait(3);
+  BaesdRatioCringedexer.Set(0);
+  NotButtShooter.Set (0);
+  
 }
 
 void Robot::AutonomousPeriodic() {
@@ -33,7 +54,9 @@ void Robot::AutonomousPeriodic() {
 
 void Robot::TeleopInit() {
 
-    PepeLeft.SetInverted (true);
+   // PepeLeft.SetInverted (true);
+
+   NotButtShooter.Set (0);
 }
 
 void Robot::TeleopPeriodic() {
@@ -50,18 +73,40 @@ void Robot::TeleopPeriodic() {
  
 
   if (Pogger1.GetX(frc::GenericHID::kLeftHand) > 0) {
-    PepeLeft.Set(speedToSet - Pogger1.GetX(frc::GenericHID::kLeftHand));
+    PepeLeft.Set(speedToSet + Pogger1.GetX(frc::GenericHID::kLeftHand) / 2);
+    PepeRight.Set(speedToSet + Pogger1.GetX(frc::GenericHID::kLeftHand) / -2);
+
   }
 
   else
   {
-    PepeLeft.Set(speedToSet - Pogger1.GetX(frc::GenericHID::kLeftHand));
+    PepeRight.Set(speedToSet - Pogger1.GetX(frc::GenericHID::kLeftHand) / 2);
+    PepeLeft.Set(speedToSet - Pogger1.GetX(frc::GenericHID::kLeftHand) / -2);
+
   }
-  
 
 
-  frc::SmartDashboard::PutNumber("Trigger Test" , speedToSet);
+  frc::SmartDashboard::PutNumber("Trigger Test", Pogger1.GetY(frc::GenericHID::kRightHand));
 
+
+  if (abs(Pogger1.GetY(frc::GenericHID::kRightHand)) < 0.1) {
+
+    NotButtShooter.Set (0);
+  }
+
+  else { 
+    NotButtShooter.Set (abs(Pogger1.GetY(frc::GenericHID::kRightHand)));
+    }
+
+  if (Pogger1.GetBumper(frc::GenericHID::kLeftHand))  {
+
+    BaesdRatioCringedexer.Set(0.7);
+  }
+
+  else {
+
+    BaesdRatioCringedexer.Set(0);
+  }
 
 }
 
@@ -77,4 +122,5 @@ void Robot::TestPeriodic() {}
 int main() {
   return frc::StartRobot<Robot>();
 }
+
 #endif
